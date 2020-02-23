3341ab63c0fdec5bab3a4d0a04c5f5a512101bcc
Soham Dasgupta <sohdasgupta@deloitte.com>
2020-02-23 15:29:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# Introduction 
+This is a small library that contains only one class `SqlConnectionProxy` which acts as a static proxy for the `SqlConnection` class. 
+This class adds functionality of logging the `Open`, `Close` and `Dispose` methods using the popular `log4net` package.
+
+# Getting Started
+1.	Build the project.
+2.	Reference the `SqlConnectionProxy.dll` in your desired project.
+3.	Make sure that you have `log4net` configured before using this.
+4.	Replace all `SqlConnection` instances with `SqlConnectionProxy`.
+
+# Test
+Use the following sql snippet to find out the leaking connections
+
+```
+WITH CTE_Log
+AS
+	(
+
+		SELECT
+			[Assembly] = C.Parsed.value('(/root/assembly)[1]', 'nvarchar(max)'),
+			[File] =	 C.Parsed.value('(/root/file)[1]', 'nvarchar(max)'),
+			[Line] =	 C.Parsed.value('(/root/line)[1]', 'int'),
+			l.Level
+			FROM
+				Log l
+				CROSS APPLY (
+					SELECT
+						CAST(l.Message AS xml)
+				) AS C (Parsed)
+			WHERE
+				l.Level IN ('SqlConnection.Close', 'SqlConnection.Open', 'SqlConnection.Dispose')
+	),
+
+CTE_Close
+AS
+	(
+		SELECT
+			l.Assembly,
+			l.[File],
+			l.Line,
+			COUNT(1) AS [CloseCount]
+			FROM
+				CTE_Log l
+			WHERE
+				l.Level = 'SqlConnection.Close'
+			GROUP BY
+				l.Assembly,
+				l.[File],
+				l.Line
+	),
+CTE_Open
+AS
+	(
+		SELECT
+			l.Assembly,
+			l.[File],
+			l.Line,
+			COUNT(1) AS [OpenCount]
+			FROM
+				CTE_Log l
+			WHERE
+				l.Level = 'SqlConnection.Open'
+			GROUP BY
+				l.Assembly,
+				l.[File],
+				l.Line
+	),
+CTE_Dispose
+AS
+	(
+		SELECT
+			l.Assembly,
+			l.[File],
+			l.Line,
+			COUNT(1) AS [DisposeCount]
+			FROM
+				CTE_Log l
+			WHERE
+				l.Level = 'SqlConnection.Dispose'
+			GROUP BY
+				l.Assembly,
+				l.[File],
+				l.Line
+	),
+CTE_Result
+AS
+	(
+		SELECT
+			o.Assembly,
+			o.[File],
+			o.Line,
+			o.OpenCount,
+			DisposeCount = ISNULL(d.DisposeCount, 0),
+			CloseCount =   ISNULL(c.CloseCount, 0),
+			[LeakCount] =  o.OpenCount - IIF(ISNULL(c.CloseCount, 0) > ISNULL(d.DisposeCount, 0), ISNULL(c.CloseCount, 0), ISNULL(d.DisposeCount, 0))
+			FROM
+				CTE_Open o
+				LEFT JOIN CTE_Close c ON c.Assembly = o.Assembly
+					AND o.[File] = c.[File]
+					AND o.Line = c.Line
+				LEFT JOIN CTE_Dispose d ON o.Assembly = d.Assembly
+					AND o.[File] = d.[File]
+					AND o.Line = d.Line
+	)
+SELECT * FROM CTE_Result WHERE LeakCount > 0
+```
+
+# Author
+Soham Dasgupta <soham1.dasgupta@gmail.com>
---
+++ SqlConnectionProxy.cs
@@ -0,0 +1,144 @@
+﻿using log4net;
+using log4net.Core;
+using System.Data.Common;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace System.Data.SqlClient
+{
+    public sealed class SqlConnectionProxy : DbConnection, IDisposable
+    {
+        private SqlConnection mConnection;
+        private ILog mLogger;
+        private string mSourceFile = null;
+        private int mLineNumber = 0;
+        private Exception mException = null;
+
+        public SqlConnectionProxy(string connectionString = null,
+            [CallerFilePath] string sourceFilePath = null,
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (!string.IsNullOrWhiteSpace(connectionString))
+                mConnection = new SqlConnection(BuildConnectionString(connectionString));
+            else
+                mConnection = new SqlConnection();
+            mLogger = LogManager.GetLogger("SqlConnectionProxy");
+            mSourceFile = Path.GetFileName(sourceFilePath);
+            mLineNumber = sourceLineNumber;
+        }
+
+        public static implicit operator SqlConnection(SqlConnectionProxy v)
+        {
+            if (v != null)
+            {
+                if (v.mConnection.State != ConnectionState.Open)
+                    v.mConnection.Open();
+                return v.mConnection;
+            }
+            return null;
+        }
+
+        public override string ConnectionString
+        {
+            get => mConnection.ConnectionString;
+            set => mConnection.ConnectionString = BuildConnectionString(value);
+        }
+
+        private static string BuildConnectionString(string value)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(value);
+            builder.Pooling = false;
+            builder.MultipleActiveResultSets = true;
+            builder.AsynchronousProcessing = true;
+            builder.ApplicationName = "LeaseWeb";
+            return builder.ConnectionString;
+        }
+
+        public override string Database => mConnection.Database;
+
+        public override string DataSource => mConnection.DataSource;
+
+        public override string ServerVersion => mConnection.ServerVersion;
+
+        public override ConnectionState State => mConnection.State;
+
+        public override void ChangeDatabase(string databaseName)
+        {
+            mConnection.ChangeDatabase(databaseName);
+        }
+
+        public override void Close()
+        {
+            Log(new Level(60000, "SqlConnection.Close"));
+            if (mConnection.State != ConnectionState.Closed)
+                mConnection.Close();
+        }
+
+        public override void Open()
+        {
+            if (mConnection.State != ConnectionState.Open)
+                mConnection.Open();
+            Log(new Level(50000, "SqlConnection.Open"));
+        }
+
+        public new void Dispose()
+        {
+            Log(new Level(70000, "SqlConnection.Dispose"));
+            mConnection.Dispose();
+        }
+
+        public new SqlTransaction BeginTransaction()
+        {
+            return BeginTransaction(IsolationLevel.ReadCommitted) as SqlTransaction;
+        }
+
+        public new SqlTransaction BeginTransaction(IsolationLevel isolationLevel)
+        {
+            return BeginTransaction(isolationLevel) as SqlTransaction;
+        }
+
+        public new SqlCommand CreateCommand()
+        {
+            return CreateCommand() as SqlCommand;
+        }
+
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        {
+            return mConnection.BeginTransaction(isolationLevel);
+        }
+
+        protected override DbCommand CreateDbCommand()
+        {
+            return mConnection.CreateCommand();
+        }
+
+        private void Log(Level level)
+        {
+            mLogger.Logger.Log(typeof(SqlConnectionProxy), level, GetMessage(), mException);
+        }
+
+        private string GetMessage()
+        {
+            return $"<root><assembly>{GetAssemblyName()}</assembly><file>{mSourceFile}</file><line>{mLineNumber}</line></root>";
+        }
+
+        private string GetAssemblyName()
+        {
+            StackTrace st = new StackTrace(4, true);
+            MethodBase m = st.GetFrame(0).GetMethod();
+            return m.DeclaringType.Assembly.GetName().Name;
+        }
+
+        public static void ClearAllPools()
+        {
+            SqlConnection.ClearAllPools();
+        }
+
+        public static void ClearPool(SqlConnection connection)
+        {
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
